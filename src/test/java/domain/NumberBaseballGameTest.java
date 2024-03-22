3b0ae6e8f4241ced90b423b05577e99be320a27b
impala <dla4051@naver.com>
2024-03-22 18:47:08 +0900

    test(domain): init 메서드 테스트 코드 작성
    기존 generateAnswer 메소드의 테스트 코드에 추가
    - 중복된 숫자가 포함되지 않는지 검증

--- src/test/java/domain/NumberBaseballGameTest.java
+++ src/test/java/domain/NumberBaseballGameTest.java
@@ -3,24 +3,32 @@ package domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.*;
 
 class NumberBaseballGameTest {
     @Test
-    @DisplayName("1~9 사이의 랜덤한 정수 3개를 생성한다")
-    void generateAnswer() {
+    @DisplayName("시스템은 게임이 시작되면 1~9사이의 서로 다른 수로 이루어진 3자리 숫자를 생성한다")
+    void init() {
         //given
         NumberBaseballGame game = new NumberBaseballGame();
 
         //when
-        game.generateAnswer();
+        game.init();
 
         //then
-        int[] answer = game.getAnswer();
+        List<Integer> answer = game.getAnswer();
 
-        assertThat(answer.length).isEqualTo(3);
+        assertThat(answer.size()).isEqualTo(3);
+
         for (int number : answer) {
             assertThat(number).isBetween(1, 9);
         }
+
+        Set<Integer> numberSet = new HashSet<>(answer);
+        assertThat(answer.size()).isEqualTo(numberSet.size());
     }
 }
