d056ac5f6ad2a142e525ac73a36d2b705f24e6aa
impala <dla4051@naver.com>
2024-03-22 17:21:03 +0900

    feat(domain): 랜덤한 정수 3개를 생성하는 메서드 작성

--- src/main/java/domain/NumberBaseballGame.java
+++ src/main/java/domain/NumberBaseballGame.java
@@ -1,4 +1,19 @@
 package domain;
 
+import java.util.Random;
+
 public class NumberBaseballGame {
+
+    private final Random random = new Random();
+    private int[] answer = new int[3];
+
+    public void generateAnswer() {
+        for (int i = 0; i < 3; i++) {
+            answer[i] =  random.nextInt(9) + 1;
+        }
+    }
+
+    public int[] getAnswer() {
+        return answer;
+    }
 }
