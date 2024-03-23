be3362a99e6e102f68eaca6806cce69a909e5bef
impala <dla4051@naver.com>
2024-03-24 03:09:16 +0900

    test(domain): judge 메소드 테스트 코드 작성
    총 9가지 케이스에 대한 입력값과 결과를 CsvSource로 받아 ParameterizedTest를 수행

--- src/test/java/domain/NumberBaseballGameTest.java
+++ src/test/java/domain/NumberBaseballGameTest.java
@@ -1,10 +1,12 @@
 package domain;
 
+import domain.dto.GameResult;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -33,4 +35,43 @@ class NumberBaseballGameTest {
         Set<Integer> numberSet = new HashSet<>(answer);
         assertThat(answer.size()).isEqualTo(numberSet.size());
     }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            // answer : input : expected(strike, ball)
+            "1,2,3 : 1,2,3 : 3,0",
+            "1,2,3 : 1,2,4 : 2,0",
+            "1,2,3 : 1,3,2 : 1,2",
+            "1,2,3 : 1,3,4 : 1,1",
+            "1,2,3 : 1,4,5 : 1,0",
+            "1,2,3 : 3,1,2 : 0,3",
+            "1,2,3 : 3,4,2 : 0,2",
+            "1,2,3 : 3,4,5 : 0,1",
+            "1,2,3 : 4,5,6 : 0,0"
+    }, delimiter = ':')
+    @DisplayName("시스템은 사용자의 입력을 받으면 정답과 비교하여 판정 결과를 반환한다")
+    void judge(String answer, String input, String expected) {
+        //given
+        NumberBaseballGame game = new NumberBaseballGame((lowerBound, upperBound, n) -> mapToInts(answer));
+        game.init();
+
+        //when
+        GameResult result = game.judge(mapToInts(input));
+
+        //then
+        List<Integer> expectedResult = mapToInts(expected);
+        assertThat(result.getStrikeCount()).isEqualTo(expectedResult.get(0));
+        assertThat(result.getBallCount()).isEqualTo(expectedResult.get(1));
+    }
+
+    
+    private static List<Integer> mapToInts(String input) {
+        String[] stringArray = input.split(",");
+        List<Integer> nums = new ArrayList<>();
+        for (String s : stringArray) {
+            nums.add(Integer.valueOf(s));
+        }
+        return nums;
+    }
+
 }
