76ec7e73a74a0a935f558f0d27fa5ea66eda3332
impala <dla4051@naver.com>
2024-03-24 02:56:41 +0900

    test(domain): random 메소드에 대한 검증을 위해 테스트 횟수 추가
    - RepeatedTest를 사용하여 테스트를 500번 반복하도록 설정

--- src/test/java/domain/NumberBaseballGameTest.java
+++ src/test/java/domain/NumberBaseballGameTest.java
@@ -1,7 +1,9 @@
 package domain;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.HashSet;
 import java.util.List;
@@ -10,7 +12,7 @@ import java.util.Set;
 import static org.assertj.core.api.Assertions.*;
 
 class NumberBaseballGameTest {
-    @Test
+    @RepeatedTest(500)
     @DisplayName("시스템은 게임이 시작되면 1~9사이의 서로 다른 수로 이루어진 3자리 숫자를 생성한다")
     void init() {
         //given
